0825adc06c7e90c13ac89ccd9e88c55b6b93e697
Robert <robert@bekwam.com>
2018-09-08 15:48:30 -0400

    Initial commit

---
+++ src/main/kotlin/RoomLayoutApp/Hello.kt
@@ -0,0 +1,6 @@
+package RoomLayoutApp
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/RoomLayoutApp/HelloTest.kt
@@ -0,0 +1,8 @@
+package RoomLayoutApp
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
